8a7fda40fca1fecbe6c5e77000444493cd0836cc
Minnur Yunusov <mmyunusov@gmail.com>
2017-04-08 12:28:44 -0700

    Add GA event (click) tracking.

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,11 @@
   $(document).ready(function() {
 
     $('.tooltip').tooltipster({theme: 'tooltipster-minnur'});
+    if (typeof ga === 'function') {
+      $('a').on('click', function(e) {
+        ga('send', 'event', 'Link', 'click', $(this).attr('href'));
+      });
+    }
 
   });
 
