5b1b2388989fc4c9f8a6bf8f1b0c6f9d8d771d19
Minnur Yunusov <mmyunusov@gmail.com>
2020-05-27 00:51:07 +0500

    Fix darkmode toggle.

--- src/scripts.js
+++ src/scripts.js
@@ -8,7 +8,6 @@
   var darkmode_class = 'darkmode';
 
   var toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-  var currentTheme = localStorage.getItem('theme');
 
   if (window.matchMedia && window.matchMedia(color_scheme).matches) {
     body.addClass(darkmode_class);
@@ -16,40 +15,31 @@
     toggleSwitch.checked = true;
   }
 
-  window.matchMedia(color_scheme).addEventListener('change', function(e) {
+  window.matchMedia(color_scheme).addListener(function(e) {
     if (e.matches) {
       body.addClass(darkmode_class);
       picture.attr('src', darkmode_image);
+      toggleSwitch.checked = true;
     }
     else {
       body.removeClass(darkmode_class);
       picture.attr('src', regular_image);
+      toggleSwitch.checked = false;
     }
   });
 
-  if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-    if (currentTheme === darkmode_class) {
-      toggleSwitch.checked = true;
-    }
-  }
-
   function switchTheme(e) {
     if (e.target.checked) {
-      document.documentElement.setAttribute('data-theme', darkmode_class);
-      localStorage.setItem('theme', darkmode_class);
       body.addClass(darkmode_class);
       picture.attr('src', darkmode_image);
     }
     else {
-      document.documentElement.setAttribute('data-theme', '');
-      localStorage.setItem('theme', '');
       body.removeClass(darkmode_class);
       picture.attr('src', regular_image);
     }    
   }
 
-  toggleSwitch.addEventListener('change', switchTheme, false);
+  toggleSwitch.addEventListener('change', switchTheme);
 
   $(document).ready(function() {
     $('.tooltip').tooltipster({theme: 'tooltipster-minnur'});
