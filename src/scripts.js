959535c53337252ee739bce4caeeab341c785ff7
Minnur Yunusov <mmyunusov@gmail.com>
2020-05-27 00:25:35 +0500

    Add dark mode toggle.

--- src/scripts.js
+++ src/scripts.js
@@ -34,4 +34,31 @@
     }
   });
 
+  var toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+  var currentTheme = localStorage.getItem('theme');
+
+  if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === darkmode_class) {
+      toggleSwitch.checked = true;
+    }
+  }
+
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', darkmode_class);
+      localStorage.setItem('theme', darkmode_class);
+      body.addClass(darkmode_class);
+      picture.attr('src', darkmode_image);
+    }
+    else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', '');
+      body.removeClass(darkmode_class);
+      picture.attr('src', regular_image);
+    }    
+  }
+
+  toggleSwitch.addEventListener('change', switchTheme, false);
+
 })(jQuery);
